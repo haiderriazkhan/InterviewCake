104e9306d8d7e1df1e34f7a79c9e8a5973d2b07d
Haider Khan <haider.riaz@mail.mcgill.ca>
2015-11-02 22:24:25 -0500

    Update MergeMeetings.java

--- MergeMeetings.java
+++ MergeMeetings.java
@@ -1,6 +1,54 @@
 public class MergeMeetings {
-  
-    
+	
+	public List<Map.Entry<Integer,Integer>> merge_times(List<Map.Entry<Integer, Integer>> calendar) {
+		
+		// We will use a TreeMap to sort the meeeting times by their start time. 			
+		
+		SortedMap<Integer, Integer> Sorted_Meetings = new TreeMap<Integer,Integer>();
+		
+		for (Map.Entry<Integer, Integer> x : calendar) {
+			
+			Sorted_Meetings.put(x.getKey() , x.getValue());
+			
+		}
+		
+		// Iterate through the TreeMap and merge the times. 
+		
+		List<Map.Entry<Integer, Integer>> Merged_Meetings = ArrayList<AbstractMap.SimpleEntry<Integer,Integer>>();
+		
+		Merged_Meetings.add(Sorted_Meetings.pollFirstEntry());
+		
+		int index = 0;
+		
+		for (Map.Entry<Integer, Integer> meeting : Merged_Meetings.entrySet()) {
+			
+			Integer start_time = meeting.getKey();
+  			Integer finish_time = meeting.getValue();
+  			
+  			Map.Entry<Integer, Integer> prev_meeting = Merged_Meetings.get(index);
+  			
+  			Integer prev_start_time = prev_meeting.getKey();
+  			Integer prev_finish_time = prev_meeting.getValue();
+  			
+  			if (start_time >= prev_start_time &&  start_time <= prev_finish_time) {
+  				
+  				if (finish_time > prev_finish_time) {
+  					
+  					Merged_Meetings.set(index, new AbstractMap.SimpleEntry<Integer,Integer>(prev_start_time, finish_time));
+  					
+  				}
+  				
+  				continue;
+  				
+  			}
+  			
+			index++;
+			
+		}
+		
+		return Merged_Meetings;
+  	  
+  	}
 
 
 

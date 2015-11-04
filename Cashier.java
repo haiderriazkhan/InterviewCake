c575256d6992e79a8e6ba3be72042b3d7db65f18
Haider Khan <haider.riaz@mail.mcgill.ca>
2015-11-03 19:52:04 -0500

    Update Cashier.java

--- Cashier.java
+++ Cashier.java
@@ -1,56 +1,41 @@
 public class cashier {
 	
-	private int counter;
-	public final int number_of_ways_to_pay;
 	
-	public Cashier(int amount, int[] denominations) {
+	// Recursive solution
+	public int count_number_of_ways(int amount, int acc, int index,  int[] denominations) {
 		
-		counter = 0;
-		
-		count_number_of_ways(amount , 0 , 0,  denominations);
-		
-		number_of_ways_to_pay = counter;
-		
-	}
-	
-	private void count_number_of_ways(int amount, int acc, int index, int[] denominations) {
-		
-		if (acc > amount) return;
+		if (acc > amount) return 0;
 		
 		if (amount == acc) {
 			
-			counter++;
-			return;
+			return 1;
 			
 		}
 		
 		
 		int len = denominations.length;
+		int count = 0;
 		
 		while (acc < amount) {
-			
 			
 		
 			for (int i = index + 1; i < len; i++) {
 			
 				
-				count_number_of_ways(amount, acc + denominations[i], index + i, denominations);
+				count += count_number_of_ways(amount, acc + denominations[i], index + i, denominations);
 				
 			
 			}
 			
 			acc += denominations[index];
 			
-			if (acc > amount) return;
+			if (acc > amount) return count + 0;
 			
-			if (acc == amount) {
+			if (acc == amount) return count + 1;
 				
-				counter++;	
-				return;
-			}
-		
-			
 		}
+		
+		return count + 0;
 		
 		
 	}

d44e9702776895dcef812e7bd992da4da1437c82
Haider Khan <haider.riaz@mail.mcgill.ca>
2015-11-14 18:38:46 -0500

    Update Love.java

--- Love.java
+++ Love.java
@@ -1,1 +1,27 @@
-
+public class Love {
+	
+	
+	private class Love_Rectangle {
+		
+		int x;
+		int y;
+		int width;
+		int height;
+		
+	}
+	
+	public Love_Rectangle strength_of_love(Love_Rectangle lover_one , Love_Rectangle lover_two) {
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}

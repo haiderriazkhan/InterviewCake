0c66733ab395fc6612f5583d2bbd60d5953831b4
Haider Khan <haider.riaz@mail.mcgill.ca>
2015-11-16 03:58:28 -0500

    Update BinaryTrees.java

--- BinaryTrees.java
+++ BinaryTrees.java
@@ -1,8 +1,21 @@
 public class BinaryTrees {
-  
-  
-  
-  
-  
+	
+	public class Node {
+		
+		Node left;
+		Node right;
+		int val;
+		
+	}
+	
+	public int height(Node node){
+
+		if(node == null) return 0;
+		return 1 + Math.max(height(node.left) ,  height(node.right));
+		
+	}
+	
+	
+	
   
 }

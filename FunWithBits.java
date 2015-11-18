90a483bb1b4c91b7a92c3bfe371631f414b8386c
Haider Khan <haider.riaz@mail.mcgill.ca>
2015-11-18 15:00:56 -0500

    Update FunWithBits.java

--- FunWithBits.java
+++ FunWithBits.java
@@ -1,1 +1,23 @@
-
+public class FunWithBits {
+	
+	// To add 1 to a number x we flip the rightmost 0 bit and all numbers after it (if there are any).
+	public int increment(int x) {
+		
+		int m = 1;
+		
+		while ((x & m) != 0) {
+			
+			x = x^m;
+			m <<= 1;
+			
+		}
+		
+		x = x^m;
+		return x;
+		
+	}
+	
+	
+	
+	
+}

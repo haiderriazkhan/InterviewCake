50d53f88156fd1ba0170f8cd787bd1f868dc1711
Haider Khan <haider.riaz@mail.mcgill.ca>
2015-11-17 22:04:12 -0500

    Update and rename LinkedList.java to LinkedListFun.java

---
+++ LinkedListFun.java
@@ -0,0 +1,19 @@
+public class LinkedListFun {
+	
+	// Cycle in a Linked List. We solve this using Floyd's cycle-finding algorithm.
+	public boolean Is_There_A_Cycle (LinkedList<?> ll) {
+		
+		
+		
+		
+	}
+	
+	public <T> find_kth_to_last_node(LinkedList<T> ll) {
+		
+		
+		
+	}
+	
+	
+	
+}

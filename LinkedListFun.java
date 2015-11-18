6396f98d6c279c41b47993094cce5b1b20914782
Haider Khan <haider.riaz@mail.mcgill.ca>
2015-11-18 02:18:19 -0500

    Update LinkedListFun.java

--- LinkedListFun.java
+++ LinkedListFun.java
@@ -1,14 +1,41 @@
 public class LinkedListFun {
 	
 	// Cycle in a Linked List. We solve this using Floyd's cycle-finding algorithm.
-	public boolean Is_There_A_Cycle (LinkedList<?> ll) {
+	public boolean Is_There_A_Cycle(Node head) {
 		
+		if (head == null) return false;
 		
+		Node slow,fast;
+		
+		slow = head;
+		fast = head;
+		
+		while (true) {
+			
+			slow = head.next;
+			
+			if (fast.next != null) {
+				
+				fast = fast.next.next;
+				
+			}
+			else {
+				
+				return false;
+				
+			}
+			
+			if (fast == null) return false;
+			
+			if (slow == fast) return true;
+			
+		}
 		
 		
 	}
 	
-	public <T> find_kth_to_last_node(LinkedList<T> ll) {
+	public <T> T find_kth_to_last_node(LinkedList<T> ll) {
+		
 		
 		
 		

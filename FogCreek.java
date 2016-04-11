caeaf3307f6968e2451863553f9d65d25a0bf9dd
haiderriazkhan <haider.riaz@mail.mcgill.ca>
2016-04-10 23:01:56 -0400

    Changes to FogCreek

--- FogCreek.java
+++ FogCreek.java
@@ -1,27 +1,40 @@
 import java.io.*;
 import java.util.*;
+import java.util.logging.Logger;
 
 
-// Sort the characters in the following string:
+/**
 
-// abcdefghijklmnopqrstuvwxyz_
+Sort the characters in the following string:
 
-// by the number of times the character appears in the following text (descending):
+abcdefghijklmnopqrstuvwxyz_
 
-// epqiiqwdiwgyka_vsqtsujeqqicnhyivo_sigwasmkwgsih_akl_gtnkhgikgveidpmt
-// qybpxpnnpbxkwpisgjmdzgh_ojysbtsnsvxvuhguocp_qc_vouxqmg_cetlpmounxnvg
-// ...
-// ...
-// ...
-// mnshz_xz
+by the number of times the character appears in the following text (descending):
 
-// Now take the sorted string, and drop all the characters after (and including) the _. The remaining word is the answer.
+epqiiqwdiwgyka_vsqtsujeqqicnhyivo_sigwasmkwgsih_akl_gtnkhgikgveidpmt
+qybpxpnnpbxkwpisgjmdzgh_ojysbtsnsvxvuhguocp_qc_vouxqmg_cetlpmounxnvg
+...
+...
+...
+mnshz_xz
 
+Now take the sorted string, and drop all the characters after (and including) the _. The remaining word is the answer.
+
+**/
 
 public class FogCreek {
 
-	public static String SortString(){
+	private static final Logger log= Logger.getLogger( FogCreek.class.getName() );
 
+	private FogCreek() {
+
+		// This is a utility class and is not meant to be instantiated.
+		// Java adds an implicit public constructor to a class that does not define at least one explicitly.
+		// Therefore, this private constructor is defined for our utility class.
+	}
+
+
+	public static String sortString() {
 
 		// HashMap to store the number of times an individual char appears in the text.
 		Map<Character, Integer> freq = new HashMap<>();
@@ -46,13 +59,13 @@ public class FogCreek {
 
 				}
 
-		    }
+			}
 
 			br.close();
 		}
 		catch (IOException e) {
 
-			System.out.println("Could not load text file.");
+			log.info("Could not load text file.");
 		}
 
 		// String that needs to be sorted.
@@ -68,7 +81,7 @@ public class FogCreek {
 		}
 
 		// Output String.
-		String sorted_pattern = "";
+		StringBuilder sorted_pattern = new StringBuilder(26);
 
 		// Building the string using our map.
 		for (Map.Entry<Integer, Character> entry : pattern_map.entrySet()) {
@@ -76,17 +89,17 @@ public class FogCreek {
 			// We stop when we hit the '_' character.
 			if (entry.getValue() == '_') break;
 
-			sorted_pattern += entry.getValue();
+			sorted_pattern.append(entry.getValue());
 
 		}
 
-		return sorted_pattern;
+		return sorted_pattern.toString();
 	}
 
 
 	public static void main(String[] args) {
 
-		System.out.println(SortString());
+		System.out.println(sortString());
 
 	}
 

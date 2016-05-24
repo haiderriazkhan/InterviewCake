9e31ab6f5103b7c5dcff4f47e13e9f8341d1b0f4
haiderriazkhan <haider.riaz@mail.mcgill.ca>
2016-05-24 01:32:36 -0400

    Added mean method

--- StreamOfNumbers.java
+++ StreamOfNumbers.java
@@ -1,35 +1,28 @@
 public class StreamOfNumbers {
 
-  public void median(Scanner in) {
+  public void median(Scanner in) throws NoSuchElementException {
 
-    try {
-      int streamLength = in.nextInt();
-    } catch (NoSuchElementException e) {
-      return;
-    }
-
+    // Read in the length of the stream
+    int streamLength = in.nextInt();
 
     if (streamLength == 0) return;
 
     int firstInt = in.nextInt();
 
     if (streamLength == 1) {
-      System.out.printf("%.1f \n",  (float) currInt );
+      System.out.printf("%.1f %n",  (float) firstInt );
       return;
     }
 
     int currInt = in.nextInt();
 
+    System.out.printf("%.1f %n",  ((float) currInt+firstInt)/2 );
+
+    // Initialize the heaps
     Queue<Integer> minHeap = new PriorityQueue<>(streamLength/2 +1);
-    Queue<Integer> maxHeap = new PriorityQueue<>(streamLength/2 +1 , new Comparator<Integer>() {
-      public int compare(int one , int two) {
-        if (one == two) {
-          return 0;
-        } else {
-          one > two ? -1 : 1;
-        }
-      }
-    });
+    Queue<Integer> maxHeap = new PriorityQueue<>(streamLength/2 +1, Collections.reverseOrder);
+
+
 
     // For the first two elements : Add smaller one to the maxHeap and bigger one to the minHeap.
     if (firstInt > currInt) {
@@ -40,10 +33,65 @@ public class StreamOfNumbers {
       maxHeap.add(firstInt);
     }
 
+    int minCounter = 1;
+    int maxCounter = 1;
+    int totalNumbersRead = 2;
+
+    while (streamLength > totalNumbersRead) {
+      currInt = in.nextInt();
+      streamLength++;
+
+      // If the current integer is smaller than the maxHeap root add it to maxHeap,
+      // else add it to minHeap
+      if (currInt < maxHeap.peek()) {
+        maxHeap.add(currInt);
+        maxCounter++;
+      } else {
+        minHeap.add(currInt);
+        minCounter++;
+      }
+
+      // Balance the heaps if required
+      if (minCounter - maxCounter > 1) {
+        maxHeap.add(minHeap.poll());
+        maxCounter++;
+        minCounter--;
+      } else if (maxCounter - minCounter > 1) {
+        minHeap.add(maxHeap.poll());
+        minCounter++;
+        maxCounter--;
+      }
+
+      // Print the median number
+
+      if (minCounter == maxCounter) {
+        System.out.printf("%.1f %n",  ((float) minHeap.peek()+maxHeap.peek())/2 );
+      } else if (minCounter > maxCounter) {
+        System.out.printf("%.1f %n",  (float) minHeap.peek() );
+      } else {
+        System.out.printf("%.1f %n",  (float) maxHeap.peek() );
+      }
+
+    }
+
 
   }
 
+  public void mean(Scanner in) throws NoSuchElementException {
 
+    // Read in the length of the stream
+    int streamLength = in.nextInt();
+
+    int sum  = 0;
+    int numCounter = 0;
+
+    while (streamLength > numCounter) {
+      sum += in.nextInt();
+      numCounter++;
+      System.out.printf("%.1f %n",  (float) sum/numCounter );
+    }
+
+  }
 
 
 
